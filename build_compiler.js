7a1dcbe371c6330b2845d69ded61a58b74478847
Chad Killingsworth <chadkillingsworth@gmail.com>
2017-10-11 13:57:02 -0500

    Publich webpack beta

--- build_compiler.js
+++ build_compiler.js
@@ -13,33 +13,33 @@ var url =
     'https://repo1.maven.org/maven2/com/google/javascript/closure-compiler/'
     + mavenVersion + '/closure-compiler-' + mavenVersion + '.jar';
 
-var shouldDownloadCompiler = true;
-var compilerJarStats = null;
-try {
-  compilerJarStats = fs.statSync('./compiler.jar');
-} catch (e) {}
-if (compilerJarStats && compilerJarStats.isFile()) {
-  var versionOutput = spawn('java',  ['-jar', 'compiler.jar', '--version']);
-  for (var line of versionOutput.output) {
-    if (line) {
-      var lineString = line.toString();
-      var versionParts = /^Version: v(\d+)$/m.exec(lineString);
-      if (versionParts) {
-        shouldDownloadCompiler = parseInt(versionParts[1], 10) < packageVer.major;
-      }
-    }
-  }
-}
-
-if (shouldDownloadCompiler) {
-   var compilerBuild = spawn('curl', ['-s', '-S', '-L', '-o', './compiler.jar', url], {
-    stdio: 'inherit'
-   });
-
-   if (compilerBuild.status !== 0) {
-    throw new Error('Downloading compiler jar from Maven Central failed');
-   }
-}
+// var shouldDownloadCompiler = true;
+// var compilerJarStats = null;
+// try {
+//   compilerJarStats = fs.statSync('./compiler.jar');
+// } catch (e) {}
+// if (compilerJarStats && compilerJarStats.isFile()) {
+//   var versionOutput = spawn('java',  ['-jar', 'compiler.jar', '--version']);
+//   for (var line of versionOutput.output) {
+//     if (line) {
+//       var lineString = line.toString();
+//       var versionParts = /^Version: v(\d+)$/m.exec(lineString);
+//       if (versionParts) {
+//         shouldDownloadCompiler = parseInt(versionParts[1], 10) < packageVer.major;
+//       }
+//     }
+//   }
+// }
+//
+// if (shouldDownloadCompiler) {
+//    var compilerBuild = spawn('curl', ['-s', '-S', '-L', '-o', './compiler.jar', url], {
+//     stdio: 'inherit'
+//    });
+//
+//    if (compilerBuild.status !== 0) {
+//     throw new Error('Downloading compiler jar from Maven Central failed');
+//    }
+// }
 
 ncp('./compiler/contrib', './contrib', function(err) {
   if (err) {
